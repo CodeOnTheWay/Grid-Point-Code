f11a7e658fb40b30bf4cb3e75f1144fcd53e910f
Pranavkumar Patel <patelpranavkumar@outlook.com>
2017-07-04 00:51:54 -0600

    test project added

--- testGPC/testGPC/Program.cs
+++ testGPC/testGPC/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GridPointCode;
 
 namespace testGPC
 {
@@ -10,6 +11,20 @@ namespace testGPC
     {
         static void Main(string[] args)
         {
+            Random Rnd = new Random();
+            double Latitude = Rnd.Next(-90, 90) + Rnd.NextDouble();
+            double Longitude = Rnd.Next(-180, 180) + Rnd.NextDouble();
+            Console.WriteLine("Latitude: " + Latitude + "\tLongitude: " + Longitude);
+
+            string GridPointCode = GPC.GetGridPointCode(Latitude, Longitude);
+            double DecodedLatitude;
+            double DecodedLongitude;
+            GPC.GetCoordinates(GridPointCode, out DecodedLatitude, out DecodedLongitude);
+
+            Console.WriteLine("GPC: " + GridPointCode);
+            Console.WriteLine("Latitude: " + DecodedLatitude + "\tLongitude: " + DecodedLongitude);
+            Console.ReadLine();
+
         }
     }
 }
